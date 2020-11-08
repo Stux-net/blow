0fc26301b80444f804a13bf69b0af602f2e6c2cd
Stuxnet <big.narkoman@mail.ru>
2020-11-09 01:46:24 +0400

    Основа

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+// let nav = document.querySelector(".nav");
+
+// nav.addEventListener("click", function (event) {
+//   let target = event.target;
+//   if (target.tagName == "A") {
+//     document.querySelector(".active").classList.remove("active");
+//     target.classList.add("active");
+//   }
+// });   // Nav active add remove !!!
